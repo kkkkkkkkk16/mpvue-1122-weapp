43dbfb4283a0aa5d24e893ee085a1f699b100b2a
kkkkkkkkk16 <2462351973@qq.com>
2019-01-05 20:56:06 +0800

    Book.vueUpdata

--- src/pages/books/Books.vue
+++ src/pages/books/Books.vue
@@ -1,17 +1,31 @@
 <template>
-    <div>
-        图书
-    </div>
+  <div>
+    <div :key="book.id" v-for="book in books">{{book.title}}</div>
+  </div>
 </template>
 
 
 
 <script>
+import { get } from "@/util";
 export default {
-
-}
+  data() {
+    return {
+      books: []
+    };
+  },
+  methods: {
+    async getList() {
+      const booklist = await get("/weapp/booklist");
+      this.books = booklist.data.data.list;
+      console.log(booklist)
+    }
+  },
+  mounted() {
+    this.getList();
+  }
+};
 </script>
 
 <style>
-
 </style>
